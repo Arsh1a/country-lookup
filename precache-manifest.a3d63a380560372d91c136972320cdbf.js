88c7c93e7a66bdbfc13b8e79f9f3c2a28070b210
Arshia <arshiafaraji22@gmail.com>
2020-06-29 12:26:06 +0430

    Updates

---
+++ precache-manifest.a3d63a380560372d91c136972320cdbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fb59bc7857633c40eac4ed4f42a6f8",
+    "url": "/country-lookup/index.html"
+  },
+  {
+    "revision": "e85eebf9be9a0a152b42",
+    "url": "/country-lookup/static/css/main.f5d03f28.chunk.css"
+  },
+  {
+    "revision": "3054f99319f39538f893",
+    "url": "/country-lookup/static/js/2.c0f5d32c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-lookup/static/js/2.c0f5d32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85eebf9be9a0a152b42",
+    "url": "/country-lookup/static/js/main.6b6d5643.chunk.js"
+  },
+  {
+    "revision": "60b8604a2c6f3f77a010",
+    "url": "/country-lookup/static/js/runtime-main.55010d06.js"
+  }
+]);
